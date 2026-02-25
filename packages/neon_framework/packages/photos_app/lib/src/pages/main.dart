ec2c642515c0f1b0cb97a1e4833e1be582d2494d
vauvenal5 <vauvenal5.ndgme@slmails.com>
2026-02-25 22:55:43 +0100

    feat(photos): add photos app
    Signed-off-by: vauvenal5 <vauvenal5.ndgme@slmails.com>

---
+++ packages/neon_framework/packages/photos_app/lib/src/pages/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PhotosMainPage extends StatelessWidget {
+  const PhotosMainPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Coming soon!'),
+      ),
+    );
+  }
+
+
+}
