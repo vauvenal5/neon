66163509fce04565c3e32ef6aa0ccced2b261d17
vauvenal5 <vauvenal5.ndgme@slmails.com>
2026-02-26 09:40:04 +0100

    feat(photos): add photos main page
    Signed-off-by: vauvenal5 <vauvenal5.ndgme@slmails.com>

--- packages/neon_framework/packages/photos_app/lib/src/pages/main.dart
+++ packages/neon_framework/packages/photos_app/lib/src/pages/main.dart
@@ -1,18 +1,214 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class PhotosMainPage extends StatelessWidget {
+import 'package:files_app/files_lib.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import 'package:neon_framework/blocs.dart';
+import 'package:neon_framework/models.dart';
+import 'package:neon_framework/sort_box.dart';
+import 'package:neon_framework/utils.dart';
+import 'package:neon_framework/widgets.dart';
+import 'package:nextcloud/webdav.dart' as webdav;
+import 'package:photos_app/src/blocs/bloc.dart';
+import 'package:photos_app/src/options.dart';
+
+class PhotosMainPage extends StatefulWidget {
   const PhotosMainPage({
     super.key,
   });
 
   @override
+  State<PhotosMainPage> createState() => _PhotosMainPageState();
+}
+
+class _PhotosMainPageState extends State<PhotosMainPage> {
+  late PhotosOptions options;
+  late PhotosBloc photosBloc;
+  late FilesBloc filesBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    options = NeonProvider.of<PhotosOptions>(context);
+    photosBloc = NeonProvider.of<PhotosBloc>(context);
+    filesBloc = NeonProvider.of<FilesBloc>(context);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Coming soon!'),
+    return Scaffold(
+      body: ValueListenableBuilder(
+        valueListenable: options.photosHomePathOption,
+        builder: (context, value, child) => CategoryView(
+          uri: value,
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryView extends StatefulWidget {
+  CategoryView({
+    required this.uri,
+    this.mimeFilter = const MimeFilter.images(),
+  }) : super(key: Key(uri.toString()));
+
+  final webdav.PathUri uri;
+  final MimeFilter mimeFilter;
+
+  @override
+  State<CategoryView> createState() => _CategoryViewState();
+}
+
+class _CategoryViewState extends State<CategoryView> {
+  late final FilesBloc filesBloc;
+  late final FilesOptions options;
+  late final FilesBrowserBloc bloc;
+  late final StreamSubscription<Object> errorsSubscription;
+  late final StickyHeaderController controller;
+
+  @override
+  void initState() {
+    filesBloc = NeonProvider.of<FilesBloc>(context);
+    options = NeonProvider.of<FilesOptions>(context);
+
+    controller = StickyHeaderController();
+
+    bloc = FilesBrowserBloc(
+      filesBloc: filesBloc,
+      options: options,
+      account: NeonProvider.of<Account>(context),
+      uri: widget.uri,
+      mode: FilesBrowserMode.browser,
+      mimeFilter: widget.mimeFilter,
+    );
+
+    errorsSubscription = bloc.errors.listen((error) {
+      if (mounted) {
+        NeonError.showSnackbar(context, error);
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    unawaited(errorsSubscription.cancel());
+    bloc.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ResultBuilder.behaviorSubject(
+      subject: bloc.files,
+      builder: (context, filesSnapshot) {
+        final sorted = filesSnapshot.data?.toList() ?? [];
+        const box = (property: FilesSortProperty.modifiedDate, order: SortBoxOrder.descending);
+        filesSortBox.sortList(sorted, box);
+
+        final categories = <String, List<webdav.WebDavFile>>{};
+
+        for (final file in sorted) {
+          final key = '${file.lastModified!.year}-${file.lastModified!.month.toString().padLeft(2, '0')}';
+          if (categories[key] == null) {
+            categories[key] = [];
+          }
+          categories[key]!.add(file);
+        }
+
+        final List<Widget> slivers =
+            categories.keys.map((key) => _buildCategory(key, categories[key]!, sorted, context)).toList();
+
+        return CustomScrollView(
+          slivers: slivers,
+          physics: const AlwaysScrollableScrollPhysics(),
+        );
+      },
+    );
+  }
+
+  Widget _buildHeader(String key, BuildContext context) {
+    return Container(
+      height: 30.0,
+      color: Theme.of(context).colorScheme.secondary,
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        key,
+        style: const TextStyle(color: Colors.white),
       ),
     );
   }
 
+  SliverStickyHeader _buildCategory(
+    String key,
+    List<webdav.WebDavFile> category,
+    List<webdav.WebDavFile> sorted,
+    BuildContext context,
+  ) {
+    final cut = 550;
+    final minAxis = 3;
+    double realdWidth = MediaQuery.sizeOf(context).width - 8;
+    final axis = minAxis + minAxis * ((realdWidth - cut) ~/ cut);
 
+    double width = (realdWidth / axis) - 4;
+
+    return SliverStickyHeader(
+      key: ValueKey(key),
+      header: _buildHeader(key, context),
+      controller: controller,
+      sliver: SliverGrid(
+        key: ValueKey('${key}_grid'),
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            final file = category[index];
+            final details = FileDetails.fromWebDav(
+              file: file,
+            );
+
+            return GestureDetector(
+              onTap: () => _onTap(file, details, sorted),
+              child: FilePreview(
+                bloc: filesBloc,
+                size: Size.square(width),
+                details: details,
+              ),
+            );
+          },
+          childCount: category.length,
+        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: axis,
+          mainAxisSpacing: 4,
+          crossAxisSpacing: 4,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _onTap(webdav.WebDavFile file, FileDetails details, List<webdav.WebDavFile> sorted) async {
+    // size warning handling needs to be made global
+    final sizeWarning = NeonProvider.of<FilesOptions>(context).downloadSizeWarning.value;
+    if (sizeWarning != null && details.size != null && details.size! > sizeWarning) {
+      final decision = await showDownloadConfirmationDialog(context, sizeWarning, details.size!);
+
+      if (!decision) {
+        return;
+      }
+    }
+
+    if (context.mounted) {
+      final capability = NeonProvider.of<AppsBloc>(context).handleAppCapability(
+        context,
+        ImageViewerCapability.fromFile(file: file, files: sorted),
+      );
+
+      if (capability != null) {
+        await capability;
+      }
+    }
+  }
 }
